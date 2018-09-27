6703433ad9d2ac8f466d280a40023a680bd2aba3
Hill <hill@kimo.com>
2018-09-28 02:23:05 +0800

    Add test_local

--- src/tag.js
+++ src/tag.js
@@ -2,23 +2,31 @@ import {client} from 'i13n-client';
 import query from 'css-query-selector';
 import {getUrl} from 'seturl'
 
-const tags = query.all('script[src*="usergram.omniscientai.com/tag.js?id"]');
+const tags = query.all('script[src*="/tag.js?id"]');
+const host = 'usergram.omniscientai.com'
 
-let thisId;
+let iniId;
+let iniPath = host+'/u';
+const getAttribute = t => v => t.getAttribute(v)
 tags.some(tag => {
-  const src = tag.getAttribute("src")
-  if (src && src.length) {
-    if (-1 !== src.indexOf('tag')) {
-      thisId = getUrl('id', src)
+  const getAttr = getAttribute(tag) 
+  const _src = getAttr("_src")
+  const tagSrc = getAttr("src")
+  const src = (_src) ? _src : tagSrc
+  if (src && src.length && -1 !== src.indexOf(host)) {
+    iniId = getUrl('id', tagSrc)
+    const _iniPath = getAttr("_iniPath")
+    if (_iniPath) {
+      iniPath = _iniPath
     }
   }
-  if (thisId) {
+  if (iniId) {
     return true
   } else {
     return false
   }
 })
 
-client(`https://usergram.omniscientai.com/u/${thisId}.ini`,
+client(`//${iniPath}/${iniId}.ini`,
   t => atob(t)
 )
