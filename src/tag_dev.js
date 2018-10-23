849832ac68f04a0533938f728db0212a0cf5ea4d
Hill <hill@kimo.com>
2018-10-23 18:17:50 +0800

    update suntory uid

--- src/tag_dev.js
+++ src/tag_dev.js
@@ -1,7 +1,7 @@
 import {client} from 'i13n-client';
 
 const reload = () => {
-  client('http://localhost:7000/src/user/rubys.ini', (t, cb) => {
+  client('http://localhost:7000/src/user/suntory.ini', (t, cb) => {
     cb(t, [
       {
         path: ['tag', 'debug', 'enabled'],
