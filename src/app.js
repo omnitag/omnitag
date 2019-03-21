f3971182e6ece6c66be1e95662fb0290a5935eb4
Hill <hill@kimo.com>
2019-03-21 15:47:20 +0800

    prevent default config not exists

--- src/app.js
+++ src/app.js
@@ -2,4 +2,4 @@ import {client} from 'i13n-client';
 import {win} from 'win-doc';
 import {getOverWrite} from './tagBase';
 
-client(win().i13nData, (configs, cb) => cb(configs, getOverWrite()));
+client(win().i13nData || {}, (configs, cb) => cb(configs, getOverWrite()));
