4362e785e33f0641851cc3c520e4bdce7db177d9
Hill <hill@kimo.com>
2020-02-26 14:21:35 +0800

    update new version

--- src/tagBase.js
+++ src/tagBase.js
@@ -4,6 +4,10 @@ const getOverWrite = () =>
     path: ['defaultMpHost'],
     value: 'https://analytics.omniscientai.com',
   },
+  {
+    path: ['omnitag', 'version'],
+    value: '2020-02-26-01'
+  }
 ];
 
 export {getOverWrite};
