47d23faeecb3f82f8d9f7d7bd661047c1caad1c9
Hill <hill@omnibpm.com>
2020-03-19 00:29:15 +0800

    wip

--- src/tagBase.js
+++ src/tagBase.js
@@ -1,3 +1,11 @@
+import i13nStore from 'i13n-store';
+import {win, doc} from 'win-doc';
+import {utils} from 'i13n-client';
+import {create, inject} from 'create-el';
+
+const {req, parseJson, get, router} = utils(); 
+
+
 const getOverWrite = () =>
 [
   {
@@ -10,4 +18,44 @@ const getOverWrite = () =>
   }
 ];
 
-export {getOverWrite};
+const parseRouter = routerData => {
+  const oRouter = new router();
+  routerData.forEach(rule => {
+    oRouter.addRoute(rule.router, ()=>{
+      const dIframe = create('iframe')()();
+      inject()(dIframe);
+      console.log({dIframe});
+    });
+  });
+  const urlPathName = doc().location.pathname;
+  let match = oRouter.match(urlPathName);
+  if (match) {
+    match.fn();
+    while ((match = match.next())) {
+      match.fn();
+    }
+  }
+}
+
+
+const interactionTask = () => {
+  const state = i13nStore.getState();
+  // const tid = state.get('tagId');
+  const tid = 'OA-c19eb6';
+  const routerUrl = `https://lan.cicd.omnicloud.tech:18000/ma_cms/get-all-routers/?tid=${tid}`;
+  req(routerUrl, oReq => e => {
+    parseRouter(
+      get(
+        parseJson(oReq.responseText),
+        ['PAYLOAD', 'data']
+      )
+    );
+  });
+};
+
+const interaction = () => {
+  i13nStore.addListener(interactionTask, 'init');
+};
+
+
+export {getOverWrite, interaction};
--- src/tag_dev.js
+++ src/tag_dev.js
@@ -1,4 +1,5 @@
 import {client, utils} from 'i13n-client';
+import {interaction} from './tagBase';
 
 // fruitpay
 // const iniUrl = '//localhost:7000/src/user/OA-1597cbff.ini';
@@ -6,37 +7,39 @@ import {client, utils} from 'i13n-client';
 // rubys.com.tw
 // const iniUrl = '//localhost:7000/src/user/rubys.ini';
 
-// cyberbiz https://www.kgcshop.com.tw/ 
+// cyberbiz https://www.kgcshop.com.tw/
 // const iniUrl = '//localhost:7000/src/user/cyberbiz.ini';
 
-const iniUrl = '//localhost:7000/src/user/OA-715e0f2a.ini';
+const iniUrl = '//lan.cicd.omnicloud.tech:7000/src/user/rubys.ini';
 
+const iniCallback = (t, cb) => {
+  cb(t, [
+    {
+      path: ['tag', 'debug', 'enabled'],
+      value: true,
+    },
+    {
+      path: ['tag', 'gtag', 'gaId'],
+      value: 'UA-124534784-1',
+    },
+    {
+      path: ['tag', 'usergram', 'enabled'],
+      value: false,
+    },
+    {
+      path: ['defaultMpHost'],
+      value: 'https://analytics.omniscientai.com',
+    },
+    {
+      path: ['tagId'],
+      value: 'OA-c1af6551',
+    },
+  ]);
+  interaction();
+};
 
 const load = () => {
-  client(iniUrl, (t, cb) => {
-    cb(t, [
-      {
-        path: ['tag', 'debug', 'enabled'],
-        value: true,
-      },
-      {
-        path: ['tag', 'gtag', 'gaId'],
-        value: 'UA-124534784-1',
-      },
-      {
-        path: ['tag', 'usergram', 'enabled'],
-        value: false,
-      },
-      {
-        path: ['defaultMpHost'],
-        value: 'https://analytics.omniscientai.com',
-      },
-      {
-        path: ['tagId'],
-        value: 'OA-c1af6551',
-      },
-    ]);
-  }, true);
+  client(iniUrl, iniCallback, true);
 };
 
 load();
@@ -45,4 +48,3 @@ window.i13nReload = () => {
   lazyAttr(iniUrl)(null);
   load();
 };
-
