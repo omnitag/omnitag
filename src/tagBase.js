95d29cd74fc300e3acad0c23688a905a1eb3cf69
Hill <hill@omnibpm.com>
2020-03-27 17:22:20 +0800

    wip

--- src/tagBase.js
+++ src/tagBase.js
@@ -3,19 +3,17 @@ import {win, doc} from 'win-doc';
 import {utils} from 'i13n-client';
 import {create, inject} from 'create-el';
 
-const {req, parseJson, get, router, getClientId} = utils(); 
+const {req, parseJson, get, router, getClientId} = utils();
 
-
-const getOverWrite = () =>
-[
+const getOverWrite = () => [
   {
     path: ['defaultMpHost'],
     value: 'https://analytics.omniscientai.com',
   },
   {
     path: ['omnitag', 'version'],
-    value: '2020-02-26-01'
-  }
+    value: '2020-02-26-01',
+  },
 ];
 
 const getTagId = () => {
@@ -26,15 +24,23 @@ const getTagId = () => {
 };
 
 const handleWebPopup = ({data, tid, cid}) => {
-      const dIframe = create('iframe')()();
-      inject()(dIframe);
-      console.log({dIframe, data});
+  const dIframe = create('iframe')()({
+    style: 'border: 0; position: fixed; top: 50%; left: 50%;' 
+  });
+  inject()(dIframe);
+  const doc = dIframe?.contentWindow?.document;
+  if (doc) {
+    doc.open('text/html', 'replace');
+    doc.write(data.html);
+    doc.close();
+  }
+  console.log({dIframe, data, doc}, dIframe.contentWindow);
 };
 
 const parseRouter = routerData => {
   const oRouter = new router();
   routerData.forEach(rule => {
-    oRouter.addRoute(rule.router, ()=>{
+    oRouter.addRoute(rule.router, () => {
       const tid = getTagId();
       const cid = getClientId();
       const wid = rule.webpopup_id;
@@ -44,7 +50,7 @@ const parseRouter = routerData => {
           data: get(parseJson(oReq.responseText), ['PAYLOAD', 'data']),
           tid,
           cid,
-          wid
+          wid,
         });
       });
     });
@@ -57,18 +63,13 @@ const parseRouter = routerData => {
       match.fn();
     }
   }
-}
+};
 
 const interactionTask = () => {
   const tid = getTagId();
   const routerUrl = `https://lan.cicd.omnicloud.tech:18000/ma_cms/get-all-routers/?tid=${tid}`;
   req(routerUrl, oReq => e => {
-    parseRouter(
-      get(
-        parseJson(oReq.responseText),
-        ['PAYLOAD', 'data']
-      )
-    );
+    parseRouter(get(parseJson(oReq.responseText), ['PAYLOAD', 'data']));
   });
 };
 
