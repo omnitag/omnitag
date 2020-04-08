be6e829ed33fd4af9f7a4da17c6d2131e81afa22
Hill <hill@kimo.com>
2020-04-08 10:18:15 +0800

    wip

--- src/tagBase.js
+++ src/tagBase.js
@@ -1,59 +1,80 @@
-import i13nStore from 'i13n-store';
-import {win, doc} from 'win-doc';
-import {utils} from 'i13n-client';
-import {create, inject} from 'create-el';
+import i13nStore from "i13n-store";
+import { win, doc } from "win-doc";
+import { utils } from "i13n-client";
+import { create, inject } from "create-el";
+import query from "css-query-selector";
+import formSerialize from "form-serialize-js";
 
-const {req, parseJson, get, router, getClientId} = utils();
+const { req, parseJson, get, router, getClientId, dispatch } = utils();
 
 const getOverWrite = () => [
   {
-    path: ['defaultMpHost'],
-    value: 'https://analytics.omniscientai.com',
+    path: ["defaultMpHost"],
+    value: "https://analytics.omniscientai.com"
   },
   {
-    path: ['omnitag', 'version'],
-    value: '2020-02-26-01',
-  },
+    path: ["omnitag", "version"],
+    value: "2020-02-26-01"
+  }
 ];
 
 const getTagId = () => {
   const state = i13nStore.getState();
   // const tid = state.get('tagId');
-  const tid = 'OA-c19eb6';
+  const tid = "OA-c19eb6";
   return tid;
 };
 
 const postIframeHeight = (win, dIframe) => {
   const dBody = win.document.body;
-  const h = dBody.offsetHeight; 
   dBody.style.margin = 0;
   dBody.style.padding = 0;
-  dIframe.style.height = h+ 'px';
-  dIframe.style.minHeight = h+ 'px';
+  dBody.style.height = "auto";
+  const h = dBody.offsetHeight;
+  console.log({ h });
+  dIframe.style.height = h + "px";
+  dIframe.style.minHeight = h + "px";
 };
 
-const initialIframe = win => {
-
+const initialIframe = ({ iframeWin, data }) => {
+  const q = query.from(iframeWin.document);
+  const fm = q.one("form");
+  if (!fm) {
+    return;
+  }
+  fm.addEventListener("submit", e => {
+    e.preventDefault();
+    const fmData = formSerialize(fm);
+    const {event_action, event_category} = data;
+    console.log({ data, fmData });
+    dispatch("action", {
+      I13N: {
+        action: event_action,
+        category: event_category,
+        label: fmData
+      },
+    });
+  });
 };
 
-const handleWebPopup = ({data, tid, cid}) => {
-  const dIframe = create('iframe')()({
-    style: 'border: 0; position: fixed; top: 50%; left: 50%;' 
+const handleWebPopup = ({ data, tid, cid }) => {
+  const dIframe = create("iframe")()({
+    style: "border: 0; position: fixed; top: 10%; left: 50%;"
   });
   inject()(dIframe);
   const iframeDoc = dIframe?.contentWindow?.document;
   const iframeWin = dIframe?.contentWindow?.window;
   if (iframeDoc) {
-    iframeDoc.open('text/html', 'replace');
+    iframeDoc.open("text/html", "replace");
     iframeDoc.write(data.html);
     iframeDoc.close();
   }
-  dIframe.onload = () => {
-    setTimeout(()=>{
-      postIframeHeight(iframeWin, dIframe);
-      initialIframe(iframeWin);
-    });
-  }
+  const execInit = () => {
+    postIframeHeight(iframeWin, dIframe);
+    initialIframe({ iframeWin, data });
+  };
+  iframeWin.onload = () => setTimeout(execInit, 500);
+  setTimeout(execInit, 1500);
 };
 
 const parseRouter = routerData => {
@@ -66,10 +87,10 @@ const parseRouter = routerData => {
       const configUrl = `https://lan.cicd.omnicloud.tech:18000/ma_cms/get-web-popup/?tid=${tid}&cid=${cid}&wid=${wid}`;
       req(configUrl, oReq => e => {
         handleWebPopup({
-          data: get(parseJson(oReq.responseText), ['PAYLOAD', 'data']),
+          data: get(parseJson(oReq.responseText), ["PAYLOAD", "data"]),
           tid,
           cid,
-          wid,
+          wid
         });
       });
     });
@@ -88,12 +109,12 @@ const interactionTask = () => {
   const tid = getTagId();
   const routerUrl = `https://lan.cicd.omnicloud.tech:18000/ma_cms/get-all-routers/?tid=${tid}`;
   req(routerUrl, oReq => e => {
-    parseRouter(get(parseJson(oReq.responseText), ['PAYLOAD', 'data']));
+    parseRouter(get(parseJson(oReq.responseText), ["PAYLOAD", "data"]));
   });
 };
 
 const interaction = () => {
-  i13nStore.addListener(interactionTask, 'init');
+  i13nStore.addListener(interactionTask, "init");
 };
 
-export {getOverWrite, interaction};
+export { getOverWrite, interaction };
