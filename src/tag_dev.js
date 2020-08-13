7e624cd11aea9bef44557d97f385c5ac1d313436
Hill <hill@kimo.com>
2020-08-13 13:07:14 +0800

    update edh

--- src/tag_dev.js
+++ src/tag_dev.js
@@ -6,13 +6,16 @@ import { interaction } from "./tagBase";
 
 // rubys.com.tw
 // const iniUrl = '//localhost:7000/src/user/rubys.ini';
-const iniUrl = "//lan.cicd.omnicloud.tech:7000/src/user/rubys-v2.ini";
+// const iniUrl = "//lan.cicd.omnicloud.tech:7000/src/user/rubys-v2.ini";
 
 // June1
 // const iniUrl = '//lan.cicd.omnicloud.tech:7000/src/user/OA-19c95c3b.ini';
 
 // shopify
 // const iniUrl = '//lan.cicd.omnicloud.tech:7000/src/user/OA-e2a4b618.ini';
+
+// edh
+const iniUrl = '//lan.cicd.omnicloud.tech:7000/src/user/OA-f8137c97.ini';
 
 const iniCallback = (t, cb) => {
   cb(t, [
