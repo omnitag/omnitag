ae2359deb492648dab3f684b86b576bbf62384b3
Hill <hill@kimo.com>
2020-09-07 18:07:38 +0800

    Add mybra

--- src/tag_dev.js
+++ src/tag_dev.js
@@ -1,12 +1,15 @@
 import { client, utils } from "i13n-client";
 import { interaction } from "./tagBase";
 
+// kfan develop
+// const iniUrl = '//lan.cicd.omnicloud.tech:7000/src/user/kfan-dev.ini';
+
 // fruitpay
 // const iniUrl = '//lan.cicd.omnicloud.tech:7000/src/user/OA-1597cbff.ini';
 
 // rubys.com.tw
 // const iniUrl = '//localhost:7000/src/user/rubys.ini';
- const iniUrl = "//lan.cicd.omnicloud.tech:7000/src/user/rubys.ini";
+// const iniUrl = "//lan.cicd.omnicloud.tech:7000/src/user/rubys.ini";
 
 // June1
 // const iniUrl = '//lan.cicd.omnicloud.tech:7000/src/user/OA-19c95c3b.ini';
