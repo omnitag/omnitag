3cf7a6a1f7d5d3f29fc6c9733751db6e8b292e78
Hank Ke <rockmanx673@gmail.com>
2021-04-14 18:57:14 +0800

    update webpopup iframe z-index

--- src/interaction.js
+++ src/interaction.js
@@ -306,7 +306,7 @@ const handleWebPopup = ({ data, tid, cid, wid }) => {
   const dIframe = create("iframe")()({
     id: "omnisegment-iframe",
     style:
-      "display: none; border: 0; position: fixed; width: 100%; top: 50%; left: 50%; transform: translate(-50%, -50%); z-index: 99",
+      "display: none; border: 0; position: fixed; width: 100%; top: 50%; left: 50%; transform: translate(-50%, -50%); z-index: 9999",
   });
   inject()(dIframe);
   const iframeDoc = dIframe?.contentWindow?.document;
