35b3b998aff328a68899ef1031093786862639fb
TingWeiKe <rockmanx673@gmail.com>
2021-07-01 14:41:54 +0800

    update datalayer

--- src/dataLayer.js
+++ src/dataLayer.js
@@ -13,13 +13,16 @@ const acceptActions = [
 ];
 
 const proccessI13nBeacon = (model, message) => {
-  const i13nData = get(message, ["i13n"], {});
-  console.log({i13nData})
-  if (i13nData.action && acceptActions.indexOf(i13nData.action) !== -1) {
-    console.log('do')
-    window.i13n.dispatch("action", {
-      I13N: i13nData,
+  const i13nData = get(message, ["i13n"]);
+  if (i13nData) {
+    window.i13n.dispatch({
+      uid: get(i13nData, ["uid"], null),
     });
+    if (i13nData.action && acceptActions.indexOf(i13nData.action) !== -1) {
+      window.i13n.dispatch("action", {
+        I13N: i13nData,
+      });
+    }
   }
 };
 
@@ -27,13 +30,16 @@ const dataLayer = () => {
   const _listener = (model, message) => {
     proccessI13nBeacon(model, message);
   };
-
-  window.dataLayer = window.dataLayer || [];
-  const helper = new DataLayerHelper(window.dataLayer, {
-    listener: _listener,
-    listenToPast: false,
-  });
-  window.i13n.helper = helper;
+  try {
+    window.dataLayer = window.dataLayer || [];
+    const helper = new DataLayerHelper(window.dataLayer, {
+      listener: _listener,
+      listenToPast: false,
+    });
+    window.i13n.helper = helper;
+  } catch (err) {
+    console.warn("Create DataLayerHelper Instance Failed:", err);
+  }
 };
 
 export default dataLayer;
